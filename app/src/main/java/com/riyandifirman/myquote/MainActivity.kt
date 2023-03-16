d2297b86263463f566f1830f6c42dfe6a9d2edec
riyandifirman <riyandi.firman08@gmail.com>
2023-03-16 13:29:39 +0700

    get data JSONArray and display it to Recycler View

--- app/src/main/java/com/riyandifirman/myquote/MainActivity.kt
+++ app/src/main/java/com/riyandifirman/myquote/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.riyandifirman.myquote
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -27,6 +28,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         getRandomQuote()
+
+        binding.btnAllQuotes.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ListQuotesActivity::class.java))
+        }
     }
 
     private fun getRandomQuote() {
